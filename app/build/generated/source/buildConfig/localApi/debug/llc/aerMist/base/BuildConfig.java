da692c8488847679cdb0f73decda3ac6bb41c0c6
Nikola <perovicnikola22@yahoo.com>
2021-05-30 19:45:24 +0200

    new commit

--- app/build/generated/source/buildConfig/localApi/debug/llc/aerMist/base/BuildConfig.java
+++ app/build/generated/source/buildConfig/localApi/debug/llc/aerMist/base/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "llc.aerMis.base";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "localApi";
-  public static final int VERSION_CODE = 9;
-  public static final String VERSION_NAME = "0.0.9";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "0.0.10";
   // Field from product flavor: localApi
   public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 }
