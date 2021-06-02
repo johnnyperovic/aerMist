008f11268bc4b8996179d6c8cfec3db8ec1338c4
Nikola <perovicnikola22@yahoo.com>
2021-06-02 14:30:29 +0200

    removed api key

---
+++ app/build/tmp/kapt3/stubs/localApiDebug/llc/aerMist/base/ui/popup/SetTimePopup.java
@@ -0,0 +1,35 @@
+package llc.aerMist.base.ui.popup;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000.\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0016J\u0012\u0010\u0005\u001a\u00020\u00042\b\u0010\u0006\u001a\u0004\u0018\u00010\u0007H\u0016J&\u0010\b\u001a\u0004\u0018\u00010\t2\u0006\u0010\n\u001a\u00020\u000b2\b\u0010\f\u001a\u0004\u0018\u00010\r2\b\u0010\u0006\u001a\u0004\u0018\u00010\u0007H\u0017J\b\u0010\u000e\u001a\u00020\u0004H\u0016\u00a8\u0006\u000f"}, d2 = {"Lllc/aerMist/base/ui/popup/SetTimePopup;", "Landroidx/fragment/app/DialogFragment;", "()V", "dismiss", "", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "onStart", "app_localApiDebug"})
+public final class SetTimePopup extends androidx.fragment.app.DialogFragment {
+    private java.util.HashMap _$_findViewCache;
+    
+    @java.lang.Override()
+    public void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.annotation.RequiresApi(value = android.os.Build.VERSION_CODES.M)
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onStart() {
+    }
+    
+    @java.lang.Override()
+    public void dismiss() {
+    }
+    
+    public SetTimePopup() {
+        super();
+    }
+}
