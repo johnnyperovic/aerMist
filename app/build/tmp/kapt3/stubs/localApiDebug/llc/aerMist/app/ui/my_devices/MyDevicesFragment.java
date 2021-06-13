005dedb652985729dbd3130aede807a6b1090e41
Nikola <perovicnikola22@yahoo.com>
2021-06-13 15:33:06 +0200

    added popup for my  devices222

--- app/build/tmp/kapt3/stubs/localApiDebug/llc/aerMist/app/ui/my_devices/MyDevicesFragment.java
+++ app/build/tmp/kapt3/stubs/localApiDebug/llc/aerMist/app/ui/my_devices/MyDevicesFragment.java
@@ -2,8 +2,8 @@ package llc.aerMist.app.ui.my_devices;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0096\u0001\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\n\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\b\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\r\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0012\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010N\u001a\u00020OJ\u0006\u0010P\u001a\u00020OJ\b\u0010Q\u001a\u00020OH\u0002J\b\u0010R\u001a\u00020OH\u0002J\u0012\u0010S\u001a\u00020O2\b\u0010T\u001a\u0004\u0018\u00010UH\u0017J&\u0010V\u001a\u0004\u0018\u00010W2\u0006\u0010X\u001a\u00020Y2\b\u0010Z\u001a\u0004\u0018\u00010[2\b\u0010T\u001a\u0004\u0018\u00010UH\u0016J\u0006\u0010\\\u001a\u00020OJ\u0006\u0010]\u001a\u00020OJ\u000e\u0010^\u001a\u00020O2\u0006\u0010_\u001a\u00020`J\u0006\u0010#\u001a\u00020OJ\u0006\u0010-\u001a\u00020OJ\u0006\u0010E\u001a\u00020OJ\u0006\u0010K\u001a\u00020OJ\u000e\u0010a\u001a\u00020O2\u0006\u0010_\u001a\u00020`J\u000e\u0010b\u001a\u00020O2\u0006\u0010_\u001a\u00020`J\u000e\u0010c\u001a\u00020O2\u0006\u0010_\u001a\u00020`J\u000e\u0010d\u001a\u00020O2\u0006\u0010_\u001a\u00020`R \u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0006\u0010\u0007\"\u0004\b\b\u0010\tR\u001a\u0010\n\u001a\u00020\u000bX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\f\u0010\r\"\u0004\b\u000e\u0010\u000fR\u0011\u0010\u0010\u001a\u00020\u0011\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0013R\u001a\u0010\u0014\u001a\u00020\u0015X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u0017\"\u0004\b\u0018\u0010\u0019R\u001a\u0010\u001a\u001a\u00020\u0005X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001b\u0010\u001c\"\u0004\b\u001d\u0010\u001eR\u001a\u0010\u001f\u001a\u00020 X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b!\u0010\"\"\u0004\b#\u0010$R\u001a\u0010%\u001a\u00020&X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\'\u0010(\"\u0004\b)\u0010*R\u001a\u0010+\u001a\u00020 X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b,\u0010\"\"\u0004\b-\u0010$R\u000e\u0010.\u001a\u00020/X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u00100\u001a\u000201X\u0082\u000e\u00a2\u0006\u0002\n\u0000R \u00102\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b3\u0010\u0007\"\u0004\b4\u0010\tR\u000e\u00105\u001a\u000206X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u00107\u001a\u000206X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001b\u00108\u001a\u0002098BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b<\u0010=\u001a\u0004\b:\u0010;R\u000e\u0010>\u001a\u00020?X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001a\u0010@\u001a\u00020\u0005X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\bA\u0010\u001c\"\u0004\bB\u0010\u001eR\u001a\u0010C\u001a\u00020 X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\bD\u0010\"\"\u0004\bE\u0010$R\u001a\u0010F\u001a\u00020&X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\bG\u0010(\"\u0004\bH\u0010*R\u001a\u0010I\u001a\u00020 X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\bJ\u0010\"\"\u0004\bK\u0010$R\u000e\u0010L\u001a\u00020MX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006e"}, d2 = {"Lllc/aerMist/app/ui/my_devices/MyDevicesFragment;", "Landroidx/fragment/app/Fragment;", "()V", "availableDevicesList", "", "Lcom/clj/fastble/data/BleDevice;", "getAvailableDevicesList", "()Ljava/util/List;", "setAvailableDevicesList", "(Ljava/util/List;)V", "bluetoothController", "Lllc/aerMist/app/helpers/BluetoothController;", "getBluetoothController", "()Lllc/aerMist/app/helpers/BluetoothController;", "setBluetoothController", "(Lllc/aerMist/app/helpers/BluetoothController;)V", "connectionStateCoordinator", "Lllc/aerMist/app/observers/NewObservableCoordinator;", "getConnectionStateCoordinator", "()Lllc/aerMist/app/observers/NewObservableCoordinator;", "deviceTotalNumber", "", "getDeviceTotalNumber", "()I", "setDeviceTotalNumber", "(I)V", "firstBleDevice", "getFirstBleDevice", "()Lcom/clj/fastble/data/BleDevice;", "setFirstBleDevice", "(Lcom/clj/fastble/data/BleDevice;)V", "firstDevice", "", "getFirstDevice", "()Ljava/lang/String;", "setFirstDevice", "(Ljava/lang/String;)V", "firstGate", "Landroid/bluetooth/BluetoothGatt;", "getFirstGate", "()Landroid/bluetooth/BluetoothGatt;", "setFirstGate", "(Landroid/bluetooth/BluetoothGatt;)V", "fourthDevice", "getFourthDevice", "setFourthDevice", "gattCallback", "Lcom/clj/fastble/callback/BleGattCallback;", "isFirstDevice", "", "list", "getList", "setList", "notifyCallback", "Lcom/clj/fastble/callback/BleNotifyCallback;", "notifyCallback2", "prefs", "Lllc/aerMist/app/shared/util/PreferenceCache;", "getPrefs", "()Lllc/aerMist/app/shared/util/PreferenceCache;", "prefs$delegate", "Lkotlin/Lazy;", "scanCallback", "Lcom/clj/fastble/callback/BleScanCallback;", "secondBleDevice", "getSecondBleDevice", "setSecondBleDevice", "secondDevice", "getSecondDevice", "setSecondDevice", "secondGate", "getSecondGate", "setSecondGate", "thirdDevice", "getThirdDevice", "setThirdDevice", "writeCallback", "Lcom/clj/fastble/callback/BleWriteCallback;", "checkConnection", "", "initBleConroller", "navigateToAvailableDevices", "navigateToMain", "onActivityCreated", "savedInstanceState", "Landroid/os/Bundle;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "readResponse", "readSecondResponse", "sendCommand", "input", "", "turnOf", "turnOfSecond", "turnOn", "turnOnSecond", "app_localApiDebug"})
-public final class MyDevicesFragment extends androidx.fragment.app.Fragment {
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u00ac\u0001\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\n\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u000e\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0013\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0010\u0012\n\u0002\b\t\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\u0006\u0010_\u001a\u00020`J\u0006\u0010a\u001a\u00020`J\b\u0010b\u001a\u00020`H\u0002J\b\u0010c\u001a\u00020`H\u0002J\u0012\u0010d\u001a\u00020`2\b\u0010e\u001a\u0004\u0018\u00010fH\u0017J\"\u0010g\u001a\u00020`2\u0006\u0010h\u001a\u00020\u00162\u0006\u0010i\u001a\u00020\u00162\b\u0010j\u001a\u0004\u0018\u00010kH\u0016J\u0012\u0010l\u001a\u00020`2\b\u0010m\u001a\u0004\u0018\u00010nH\u0016J&\u0010o\u001a\u0004\u0018\u00010n2\u0006\u0010p\u001a\u00020q2\b\u0010r\u001a\u0004\u0018\u00010s2\b\u0010e\u001a\u0004\u0018\u00010fH\u0016J\u0006\u0010t\u001a\u00020`J\u0006\u0010u\u001a\u00020`J\u0006\u0010v\u001a\u00020`J\u0006\u0010w\u001a\u00020`J\u000e\u0010x\u001a\u00020`2\u0006\u0010y\u001a\u00020zJ\u0006\u0010$\u001a\u00020`J\u0006\u00101\u001a\u00020`J\u0006\u0010{\u001a\u00020`J\u0006\u0010P\u001a\u00020`J\u0006\u0010Y\u001a\u00020`J\u0016\u0010|\u001a\u00020`2\u0006\u0010}\u001a\u00020\u00062\u0006\u0010~\u001a\u00020\u0016J\u000e\u0010\u007f\u001a\u00020`2\u0006\u0010y\u001a\u00020zJ\u000f\u0010\u0080\u0001\u001a\u00020`2\u0006\u0010y\u001a\u00020zJ\u000f\u0010\u0081\u0001\u001a\u00020`2\u0006\u0010y\u001a\u00020zJ\u000f\u0010\u0082\u0001\u001a\u00020`2\u0006\u0010y\u001a\u00020zR \u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00060\u0005X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0007\u0010\b\"\u0004\b\t\u0010\nR\u001a\u0010\u000b\u001a\u00020\fX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u000e\"\u0004\b\u000f\u0010\u0010R\u0011\u0010\u0011\u001a\u00020\u0012\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0014R\u001a\u0010\u0015\u001a\u00020\u0016X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0017\u0010\u0018\"\u0004\b\u0019\u0010\u001aR\u001a\u0010\u001b\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001c\u0010\u001d\"\u0004\b\u001e\u0010\u001fR\u001a\u0010 \u001a\u00020!X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\"\u0010#\"\u0004\b$\u0010%R\u001a\u0010&\u001a\u00020\'X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b(\u0010)\"\u0004\b*\u0010+R\u001a\u0010,\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b-\u0010\u001d\"\u0004\b.\u0010\u001fR\u001a\u0010/\u001a\u00020!X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b0\u0010#\"\u0004\b1\u0010%R\u001a\u00102\u001a\u00020\'X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b3\u0010)\"\u0004\b4\u0010+R\u000e\u00105\u001a\u000206X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u00107\u001a\u000208X\u0082\u000e\u00a2\u0006\u0002\n\u0000R \u00109\u001a\b\u0012\u0004\u0012\u00020\u00060\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b:\u0010\b\"\u0004\b;\u0010\nR\u000e\u0010<\u001a\u00020=X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010>\u001a\u00020=X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010?\u001a\u00020=X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010@\u001a\u00020=X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001b\u0010A\u001a\u00020B8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\bE\u0010F\u001a\u0004\bC\u0010DR\u000e\u0010G\u001a\u00020HX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010I\u001a\u00020JX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001a\u0010K\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\bL\u0010\u001d\"\u0004\bM\u0010\u001fR\u001a\u0010N\u001a\u00020!X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\bO\u0010#\"\u0004\bP\u0010%R\u001a\u0010Q\u001a\u00020\'X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\bR\u0010)\"\u0004\bS\u0010+R\u001a\u0010T\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\bU\u0010\u001d\"\u0004\bV\u0010\u001fR\u001a\u0010W\u001a\u00020!X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\bX\u0010#\"\u0004\bY\u0010%R\u001a\u0010Z\u001a\u00020\'X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b[\u0010)\"\u0004\b\\\u0010+R\u000e\u0010]\u001a\u00020^X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0083\u0001"}, d2 = {"Lllc/aerMist/app/ui/my_devices/MyDevicesFragment;", "Landroidx/fragment/app/Fragment;", "Landroid/view/View$OnClickListener;", "()V", "availableDevicesList", "", "Lcom/clj/fastble/data/BleDevice;", "getAvailableDevicesList", "()Ljava/util/List;", "setAvailableDevicesList", "(Ljava/util/List;)V", "bluetoothController", "Lllc/aerMist/app/helpers/BluetoothController;", "getBluetoothController", "()Lllc/aerMist/app/helpers/BluetoothController;", "setBluetoothController", "(Lllc/aerMist/app/helpers/BluetoothController;)V", "connectionStateCoordinator", "Lllc/aerMist/app/observers/NewObservableCoordinator;", "getConnectionStateCoordinator", "()Lllc/aerMist/app/observers/NewObservableCoordinator;", "deviceTotalNumber", "", "getDeviceTotalNumber", "()I", "setDeviceTotalNumber", "(I)V", "firstBleDevice", "getFirstBleDevice", "()Lcom/clj/fastble/data/BleDevice;", "setFirstBleDevice", "(Lcom/clj/fastble/data/BleDevice;)V", "firstDevice", "", "getFirstDevice", "()Ljava/lang/String;", "setFirstDevice", "(Ljava/lang/String;)V", "firstGate", "Landroid/bluetooth/BluetoothGatt;", "getFirstGate", "()Landroid/bluetooth/BluetoothGatt;", "setFirstGate", "(Landroid/bluetooth/BluetoothGatt;)V", "fourthBleDevice", "getFourthBleDevice", "setFourthBleDevice", "fourthDevice", "getFourthDevice", "setFourthDevice", "fourthGate", "getFourthGate", "setFourthGate", "gattCallback", "Lcom/clj/fastble/callback/BleGattCallback;", "isFirstDevice", "", "list", "getList", "setList", "notifyCallback", "Lcom/clj/fastble/callback/BleNotifyCallback;", "notifyCallback2", "notifyCallback3", "notifyCallback4", "prefs", "Lllc/aerMist/app/shared/util/PreferenceCache;", "getPrefs", "()Lllc/aerMist/app/shared/util/PreferenceCache;", "prefs$delegate", "Lkotlin/Lazy;", "renameDeviceDialog", "Lllc/aerMist/app/ui/popup/RenameDevicePopup;", "scanCallback", "Lcom/clj/fastble/callback/BleScanCallback;", "secondBleDevice", "getSecondBleDevice", "setSecondBleDevice", "secondDevice", "getSecondDevice", "setSecondDevice", "secondGate", "getSecondGate", "setSecondGate", "thirdBleDevice", "getThirdBleDevice", "setThirdBleDevice", "thirdDevice", "getThirdDevice", "setThirdDevice", "thirdGate", "getThirdGate", "setThirdGate", "writeCallback", "Lcom/clj/fastble/callback/BleWriteCallback;", "checkConnection", "", "initBleConroller", "navigateToAvailableDevices", "navigateToMain", "onActivityCreated", "savedInstanceState", "Landroid/os/Bundle;", "onActivityResult", "requestCode", "resultCode", "data", "Landroid/content/Intent;", "onClick", "id", "Landroid/view/View;", "onCreateView", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "readFourthResponse", "readResponse", "readSecondResponse", "readThirdResponse", "sendCommand", "input", "", "setOnClickListener", "showRenameDialog", "bleDevice", "positon", "turnOf", "turnOfSecond", "turnOn", "turnOnSecond", "app_localApiDebug"})
+public final class MyDevicesFragment extends androidx.fragment.app.Fragment implements android.view.View.OnClickListener {
     private final kotlin.Lazy prefs$delegate = null;
     @org.jetbrains.annotations.NotNull()
     public llc.aerMist.app.helpers.BluetoothController bluetoothController;
@@ -30,12 +30,23 @@ public final class MyDevicesFragment extends androidx.fragment.app.Fragment {
     public com.clj.fastble.data.BleDevice secondBleDevice;
     @org.jetbrains.annotations.NotNull()
     public android.bluetooth.BluetoothGatt secondGate;
+    @org.jetbrains.annotations.NotNull()
+    public com.clj.fastble.data.BleDevice thirdBleDevice;
+    @org.jetbrains.annotations.NotNull()
+    public android.bluetooth.BluetoothGatt thirdGate;
+    @org.jetbrains.annotations.NotNull()
+    public com.clj.fastble.data.BleDevice fourthBleDevice;
+    @org.jetbrains.annotations.NotNull()
+    public android.bluetooth.BluetoothGatt fourthGate;
     private boolean isFirstDevice = true;
+    private llc.aerMist.app.ui.popup.RenameDevicePopup renameDeviceDialog;
     private final com.clj.fastble.callback.BleScanCallback scanCallback = null;
     private final com.clj.fastble.callback.BleGattCallback gattCallback = null;
     private final com.clj.fastble.callback.BleWriteCallback writeCallback = null;
     private final com.clj.fastble.callback.BleNotifyCallback notifyCallback = null;
     private final com.clj.fastble.callback.BleNotifyCallback notifyCallback2 = null;
+    private final com.clj.fastble.callback.BleNotifyCallback notifyCallback3 = null;
+    private final com.clj.fastble.callback.BleNotifyCallback notifyCallback4 = null;
     private java.util.HashMap _$_findViewCache;
     
     private final llc.aerMist.app.shared.util.PreferenceCache getPrefs() {
@@ -153,6 +164,42 @@ public final class MyDevicesFragment extends androidx.fragment.app.Fragment {
     android.bluetooth.BluetoothGatt p0) {
     }
     
+    @org.jetbrains.annotations.NotNull()
+    public final com.clj.fastble.data.BleDevice getThirdBleDevice() {
+        return null;
+    }
+    
+    public final void setThirdBleDevice(@org.jetbrains.annotations.NotNull()
+    com.clj.fastble.data.BleDevice p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.bluetooth.BluetoothGatt getThirdGate() {
+        return null;
+    }
+    
+    public final void setThirdGate(@org.jetbrains.annotations.NotNull()
+    android.bluetooth.BluetoothGatt p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.clj.fastble.data.BleDevice getFourthBleDevice() {
+        return null;
+    }
+    
+    public final void setFourthBleDevice(@org.jetbrains.annotations.NotNull()
+    com.clj.fastble.data.BleDevice p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.bluetooth.BluetoothGatt getFourthGate() {
+        return null;
+    }
+    
+    public final void setFourthGate(@org.jetbrains.annotations.NotNull()
+    android.bluetooth.BluetoothGatt p0) {
+    }
+    
     @org.jetbrains.annotations.Nullable()
     @java.lang.Override()
     public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
@@ -200,6 +247,12 @@ public final class MyDevicesFragment extends androidx.fragment.app.Fragment {
     public final void readSecondResponse() {
     }
     
+    public final void readThirdResponse() {
+    }
+    
+    public final void readFourthResponse() {
+    }
+    
     private final void navigateToAvailableDevices() {
     }
     
@@ -216,6 +269,23 @@ public final class MyDevicesFragment extends androidx.fragment.app.Fragment {
     }
     
     public final void setFourthDevice() {
+    }
+    
+    public final void setOnClickListener() {
+    }
+    
+    @java.lang.Override()
+    public void onClick(@org.jetbrains.annotations.Nullable()
+    android.view.View id) {
+    }
+    
+    public final void showRenameDialog(@org.jetbrains.annotations.NotNull()
+    com.clj.fastble.data.BleDevice bleDevice, int positon) {
+    }
+    
+    @java.lang.Override()
+    public void onActivityResult(int requestCode, int resultCode, @org.jetbrains.annotations.Nullable()
+    android.content.Intent data) {
     }
     
     public MyDevicesFragment() {
